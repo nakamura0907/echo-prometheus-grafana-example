b04db5598bdba6792e2ccf6f9a844ecde7de2af6
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-02-19 16:27:20 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Echo Prometheus Grafana Example
+
+## Getting Started
+
+```bash
+$ docker compose up
+# access http://localhost:3000
+```
+
+username: admin
+password: admin
+
+`Connections` -> `Add new connection` -> `Prometheus` -> `Add new data source`
+
+- Connection: http://prometheus:9090
+
+`Dashboards` -> `Import` -> `Upload dashboard JSON file` -> `Load`
---
+++ app/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo-contrib/echoprometheus"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(echoprometheus.NewMiddleware("myapp"))
+	e.GET("/metrics", echoprometheus.NewHandler())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
